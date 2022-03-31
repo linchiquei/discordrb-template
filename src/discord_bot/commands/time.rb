1185e6ff5a40f1c0bf34278e2a8be2b33a1ac288
ricky lin <ricky@bitoex.com>
2022-03-31 22:28:46 +0800

    hotfix time version 0.1.3 bug

--- src/discord_bot/commands/time.rb
+++ src/discord_bot/commands/time.rb
@@ -4,7 +4,7 @@ class DiscordBot::Commands::Time
   end
 
   def call
-    event.respond "現在時間 #{Time.now.strftime('%H:%M:%S')}"
+    event.respond "現在時間 #{Time.zone.strftime('%H:%M:%S')}"
   end
 
   private
