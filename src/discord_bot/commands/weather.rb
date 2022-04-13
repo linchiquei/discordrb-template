87b0a36820d25ae8263effdd09f6baee59d9eca6
ricky lin <ricky@bitoex.com>
2022-04-13 23:50:40 +0800

    fix sometime image cant show

--- src/discord_bot/commands/weather.rb
+++ src/discord_bot/commands/weather.rb
@@ -3,6 +3,7 @@ require "net/http"
 require "erb"
 require "./lib/mapping"
 require "./lib/json_clean"
+require "byebug"
 
 class DiscordBot::Commands::Weather
   include ERB::Util
@@ -20,14 +21,21 @@ class DiscordBot::Commands::Weather
 
     if res.is_a?(Net::HTTPSuccess)
       clean_data = JsonClean.get_weather_params(res.body)
-      event.channel.send_embed do |embed|
+      url = Mapping.get_weather_icon(clean_data.state)
+    
+      while url.blank?
+        false
+      end
+        embed = Discordrb::Webhooks::Embed.new
         embed.colour = 0xff8040
         embed.title = "**#{clean_data.local}**"
-        embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: Mapping.get_weather_icon(clean_data.state))
+        embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: url)
         embed.add_field name: "現在狀態 :", value: clean_data.state.to_s
         embed.add_field name: "下雨機率 :", value: "#{clean_data.rain} %"
         embed.add_field name: "氣溫介於 :", value: "#{clean_data.min} ~ #{clean_data.max} 度之間"
-      end
+
+        event.send_embed(nil, embed)
+      
     else
       event.respond "派七摟！等等再試背"
     end
