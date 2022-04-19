d8f7028b32dc6b87f4907d91be3e3402d85467a9
ricky lin <ricky@bitoex.com>
2022-04-19 23:10:12 +0800

    add back

--- src/discord_bot/commands/help.rb
+++ src/discord_bot/commands/help.rb
@@ -11,7 +11,8 @@ class DiscordBot::Commands::Help
     embed.add_field name: "!time", value: "取得現在時間"
     embed.add_field name: "!guess", value: "觸發開始玩終極密碼"
     embed.add_field name: "!weather 台北", value: "取得當下台北天氣，或輸入其他縣市，取得其他縣市天氣"
-    event.send_embed(nil, embed)
+    message = event.send_embed(nil, embed)
+    message.react CROSS_MARK
   end
 
   private
