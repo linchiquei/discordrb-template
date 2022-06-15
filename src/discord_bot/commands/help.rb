c6ac2c4f18606dfed186195874d938a670c1035c
ricky <ricky@psp-power.com.tw>
2022-06-15 23:17:50 +0800

    update help

--- src/discord_bot/commands/help.rb
+++ src/discord_bot/commands/help.rb
@@ -11,7 +11,7 @@ class DiscordBot::Commands::Help
     embed.add_field name: "!time", value: "取得現在時間"
     embed.add_field name: "!guess", value: "觸發開始玩終極密碼"
     embed.add_field name: "!weather 台北", value: "取得當下台北天氣，或輸入其他縣市，取得其他縣市天氣"
-    embed.add_field name: "!covid", value: "取得台灣疫情情況"
+    embed.add_field name: "!covid", value: "取得台灣疫情情況(請等一下要爬比較久)"
     message = event.send_embed(nil, embed)
     message.react CROSS_MARK
   end
