d9faf3993223a757bca9ca0a82ffeb64cf08ba32
ricky <ricky@psp-power.com.tw>
2022-07-18 15:53:59 +0800

    close job

--- src/cron.rb
+++ src/cron.rb
@@ -16,7 +16,7 @@ module Cron
 
   # 正式站測試機器人頻道
   @scheduler.every '20s' do
-    Handlers::WahahaHandler.run(@bot)
+    #Handlers::WahahaHandler.run(@bot)
     #@bot.send_message("961525720444727336", "cron job test")
   end
 
